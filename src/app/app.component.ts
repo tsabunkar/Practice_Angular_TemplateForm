1320c626fb6ad77bf6a5659bd6da07063743f825
tsabunkar <tsabunkar@gmail.com>
2018-01-06 20:42:39 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Customer} from "./customer";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  customer: Customer= new Customer();
+
+  save(customerForm: NgForm) {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
 }
